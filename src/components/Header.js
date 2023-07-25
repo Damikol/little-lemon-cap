83adca18e493058a1be15426190b72607f370fe4
Damikol <vdamilola22@gmail.com>
2023-07-25 13:47:00 +0100

    added components file and modified appjs and index

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <img src={require('')} alt="Little Lemon Logo" />
+        </header>
+    );
+};
+
+
+export default Header;
