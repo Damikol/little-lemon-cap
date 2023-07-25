1518621bf93fd20e867a06c13c46b5f4fcb01904
Damikol <vdamilola22@gmail.com>
2023-07-25 14:33:31 +0100

    added styles

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import Header from './components/Header';
 import Nav from './components/Nav';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import './styles.css';
 
 
 function App() {
   return (
-    <>
+    <div className="container">
     <Header />
     <Nav />
     <Main />
     <Footer />
-    </>
+    </div>
   );
 }
 
